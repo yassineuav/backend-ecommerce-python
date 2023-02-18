ed50f693b3a945811b77e7ec348edef6e9333835
Yassine <yassineuav@gmail.com>
2023-02-18 23:57:07 +0100

    models

--- storefront/settings.py
+++ storefront/settings.py
@@ -84,8 +84,15 @@ INTERNAL_IPS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_store',
+        'USER': 'root',
+        'PASSWORD': 'Yassine123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
